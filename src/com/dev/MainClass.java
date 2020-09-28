8af25648dacff474ec9da0ddf630b5e0108059eb
maheswari06 <dmaheshwaripriya@gmail.com>
2020-09-28 18:07:32 +0530

    updated

--- src/com/dev/MainClass.java
+++ src/com/dev/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("First java code");
-System.out.println("Second java code");
+System.out.println("Second");
 	}
 
 }
